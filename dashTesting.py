7e191332e19ece3c1504885f132547f1b0e65101
ronoys <38772534+ronoys@users.noreply.github.com>
2019-05-25 20:22:55 -0400

    Update dashTesting.py

--- dashTesting.py
+++ dashTesting.py
@@ -7,27 +7,48 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
 
-    html.H1(children='Hello '),
+    html.H1(children='Transportation Safety Calculator '),
 
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': 'Montreal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='NYC'
-    ),
-    html.Div(id='output-container'),
 
     dcc.Slider(
-            id = 'my-slider',
-            min=0,
+            id = 'age-slider',
+            min=13,
+            max=19,
+            step=1,
+            value=13,
+            ),
+
+    html.Div(id='age-slider-output-container'),
+
+    dcc.Dropdown(
+        id='gender-dropdown',
+        options=[
+            {'label': 'Male', 'value': 'male'},
+            {'label': 'Female', 'value': 'female'}
+
+        ],
+        value='male'
+    ),
+    html.Div(id='gender-output-container'),
+
+    dcc.Slider(
+            id = 'people-slider',
+            min=1,
             max=10,
-            step=0.5,
+            step=1,
             value=-3,
             ),
-    html.Div(id='slider-output-container')
+    html.Div(id='people-slider-output-container'),
+
+    dcc.RadioItems(
+    options=[
+        {'label': 'New Driver (< 6 months)', 'value': 'true'},
+        {'label': 'Experienced Driver', 'value': 'wrong'}
+    ],
+    value='true'
+    ),
+    html.Div(id='gender-output-container')
+
 
 ])
 
@@ -37,25 +58,25 @@ app.layout = html.Div([
 
 
 @app.callback(
-    dash.dependencies.Output('slider-output-container', 'children'),
-    [dash.dependencies.Input('my-slider', 'value')])
+    dash.dependencies.Output('age-slider-output-container', 'children'),
+    [dash.dependencies.Input('age-slider', 'value')])
 
 def update_output_slider(value):
     return 'Output: "{}"'.format(value)
 
 @app.callback(
-    dash.dependencies.Output('output-container', 'children'),
-    [dash.dependencies.Input('my-dropdown', 'value')])
+    dash.dependencies.Output('gender-output-container', 'children'),
+    [dash.dependencies.Input('gender-dropdown', 'value')])
 
 def update_output_slider(value):
-    if value == 'NYC':
-        return 'gey'
-    elif value == 'MTL':
-        return 'feg'
-    else:
-        return 'Output: {}'.format(value)
+    return format(value)
 
+@app.callback(
+    dash.dependencies.Output('people-slider-output-container', 'children'),
+    [dash.dependencies.Input('people-slider', 'value')])
 
+def update_age_slider(value):
+    return format(value)
 
 
 if __name__ == '__main__':
